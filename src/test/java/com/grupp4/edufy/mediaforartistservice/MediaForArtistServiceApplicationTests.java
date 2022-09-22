b13181de34f449eea6581155440d6230107df3ee
MarcusFriberg <marcusfriberg78@icloud.com>
2022-09-22 23:35:34 +0200

    Initial commit

---
+++ src/test/java/com/grupp4/edufy/mediaforartistservice/MediaForArtistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp4.edufy.mediaforartistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaForArtistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
